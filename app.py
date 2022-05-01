027b7acf23fb81bc187d1e861877de54c63776dd
Ellana42 <mtmrkaploun@gmail.com>
2022-05-01 19:40:29 +0200

    Added search form

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from forms import SearchForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    search_form = SearchForm()
+    return render_template('home.html', form=search_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
